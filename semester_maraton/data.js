1cf5841e770031f8312624c5ad451833bd9a9f67
Wira07 <wiralodrasaputra07@gmail.com>
2023-04-02 20:50:18 +0700

    semester maraton

---
+++ semester_maraton/data.js
@@ -0,0 +1,17 @@
+const nama = "Wira Sukma Saputra";
+let usia = 5;
+
+function Biodata() {
+  if (usia == 21) {
+    // ini adalah kondisi pertama
+    console.log("anda dewasa");
+  } else {
+    // ini adalah kondisi tidak sama
+    console.log("anda masih muda");
+  }
+  //   console.log(`Halo nama saya ${nama} saya berusia ${usia}`);
+}
+Biodata();
+
+console.log(nama);
+console.log(usia);
